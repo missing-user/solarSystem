a54cde211ac042537557db14aeb1bb2c225b276b
missing-user <philipp.jurasic@gmx.de>
2020-04-30 23:38:38 +0200

    working, real planetary orbits

--- scripts/planet.js
+++ scripts/planet.js
@@ -1,7 +1,8 @@
-function Planet(pos, vel, mass, rad, name) {
+function Planet(pos, vel, mass, rad, name, color) {
 	this.p = pos || new Vector(0, 0, 0);
 	this.v = vel || new Vector(0, 0, 0);
 	this.r = rad || 1;
 	this.m = mass || 1;
 	this.name = name;
+	this.color = color;
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,57 +1,25 @@
 var canvas = document.getElementById("canvas"),
 	ctx = canvas.getContext("2d");
-
 // Set initial step size.
 var dt = 1e-1,
 	adjusted_dt = [];
-
 // Set minimal step size.
-var dx_min = 2e-2;
-var dx_max = 5e-1;
-
+var dx_min = 1e3;
+var dx_max = 5e4;
 // Set relative change tolerances.
-var x_tol = 5e-2;
-
+var x_tol = 3e-3;
 //constants
-//const G = -6.67408e-11;
-const G = -6.67408e-3;
+const G = -6.67408e-11;
+//const G = -6.67408e-14; //when using km and kg as units
+//const G = -6.67408e-3;
 var planets = [];
-
-function rk4(y, x, dx, f) {
-	var k1 = dx * f(x, y),
-		k2 = dx * f(x + dx / 2.0, y + k1 / 2.0),
-		k3 = dx * f(x + dx / 2.0, y + k2 / 2.0),
-		k4 = dx * f(x + dx, y + k3);
-	return y + (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0;
-}
-function adaptive_rk4(y, x, dx, f) {
-	//regular runge kutta step
-	var step = rk4(y, x, dx, f);
-	//half runge kutta step
-	var half_step = (double_step = rk4(y, x, dx * 0.5, f)) * 2;
-	//double runge kutta step
-	var double_step = rk4(y, x, dx * 2, f) * 0.5;
-
-	if (Math.abs(half_step - step) > x_tol) {
-		dt = dx_h;
-		console.log(
-			"step halved",
-			dt,
-			"cause the dif was",
-			(half_step - step).toExponential()
-		);
-	}
-	if (Math.abs(double_step - step) < x_tol) {
-		dt = dx_d;
-		console.log(
-			"step doubled",
-			dt,
-			"cause the dif was",
-			(double_step - step).toExponential()
-		);
-	}
-	return step;
-}
+//rendering
+var origin = {
+	scale: 1.3889477023997076e-10,
+	x: 0,
+	y: 0,
+	planet_scale: 500
+};
 
 function getGravAcc(pos, p1) {
 	// NOTE: P1 is the planet being accelerated, ignores itself
@@ -65,7 +33,6 @@ function getGravAcc(pos, p1) {
 		}
 	return resultVector;
 }
-
 //rk4 second order and with vectors
 function rk4_v2(p1, dx) {
 	k1_p = p1.v;
@@ -79,7 +46,6 @@ function rk4_v2(p1, dx) {
 	//k4
 	k4_p = p1.v.add(k3_v.multiply(dx));
 	k4_v = getGravAcc(p1.p.add(k3_p.multiply(dx)), p1);
-
 	// y + (k1 + 2*k2 + 2*k3 + k4) / 6          with vector objects
 	return {
 		new_p: k1_p
@@ -104,13 +70,11 @@ function adaptive_rk4_v2(p1, dx) {
 	//double runge kutta step
 	var double_step = rk4_v2(p1, dx * 2);
 	double_step.new_v = double_step.new_v.multiply(0.5);
-
 	if (half_step.new_v.subtract(step.new_v).length2 > x_tol)
 		adjusted_dt.push(dx * 0.5);
 	else if (double_step.new_v.subtract(step.new_v).length2 < x_tol)
 		adjusted_dt.push(dx * 2);
 	else adjusted_dt.push(dx);
-
 	//set the planets position and velocity
 	p1.p = p1.p.add(step.new_p.multiply(dx));
 	p1.v = p1.v.add(step.new_v.multiply(dx));
@@ -123,55 +87,147 @@ function adjustTimestep() {
 		if (new_dt < dt) dt = new_dt;
 	}
 	adjusted_dt = []; //clear the array
-
 	//check that the new timestep is within allowed bounds
 	if (dt > dx_max) dt = dx_max;
 	if (dt < dx_min) dt = dx_min;
 }
-
-var sun = new Planet(new Vector(300, 300, 0));
-//sun.m = 1.989e30;
-sun.m = 1.989e5;
-var earth = new Planet(
-	new Vector(-1.192693180463659e8, -9.196938727630605e7, 4.536886832430959e3),
-	new Vector(1.771605681005595e1, -2.370187334742887e1, -3.939447511491778e-5),
-	5.97219e24
-);
-earth = new Planet(new Vector(200, 200, 1), new Vector(3, 0, 0), 1e3);
-var mars = new Planet(
-	new Vector(2.240677809234498e7, -2.146888173319176e8, -5.048212889226571e6),
-	new Vector(2.501421553891083e1, 4.59861352288849, -5.173266156648491e-1),
-	6.4171e23
-);
-mars = new Planet(new Vector(350, 300, 2), new Vector(4, 5, 0), 2e2);
-var venus = new Planet(new Vector(200, 400, 0), new Vector(0, -3.5, 0), 2e1);
-planets.push(mars);
-planets.push(venus);
+var sun = new Planet();
+sun.m = 1.989e30;
+sun.r = 19911000;
+//sun.r = 696340000;
+sun.color = "yellow";
+sun.name = "sun";
 planets.push(sun);
-planets.push(earth);
+
+planets.push(
+	new Planet(
+		new Vector(5.177209740472153e7, 4.939923994023345e6, -4.345603855520848e6),
+		new Vector(-1.40374285392326e1, 5.067180631659721e1, 5.428341406032729),
+		3.302e23,
+		2439700,
+		"mercury ",
+		"gray"
+	)
+);
+planets.push(
+	new Planet(
+		new Vector(-1.044788797746455e8, -2.622227268798453e7, 5.669321489319219e6),
+		new Vector(8.296647812076673, -3.412412961250397e1, -9.470189522389862e-1),
+		48.685e23,
+		6051800,
+		"venus",
+		"violet"
+	)
+);
+planets.push(
+	new Planet(
+		new Vector(-1.192693180463659e8, -9.196938727630605e7, 4.536886832430959e3),
+		new Vector(
+			1.771605681005595e1,
+			-2.370187334742887e1,
+			-3.939447511491778e-5
+		),
+		5.97219e24,
+		6371000,
+		"earth",
+		"blue"
+	)
+);
+planets.push(
+	new Planet(
+		new Vector(2.240677809234498e7, -2.146888173319176e8, -5.048212889226571e6),
+		new Vector(2.501421553891083e1, 4.59861352288849, -5.173266156648491e-1),
+		6.4171e23,
+		3389000,
+		"mars",
+		"red"
+	)
+);
+planets.push(
+	new Planet(
+		new Vector(2.076997311919959e8, -7.472455127748314e8, -1.543338631093383e6),
+		new Vector(1.244399043430969e1, 4.119522235328096, -2.955443711777739e-1),
+		1898.13e24,
+		69911000,
+		"jupiter",
+		"orange"
+	)
+);
+planets.push(
+	new Planet(
+		new Vector(6.524934181679767e8, -1.349738177009573e9, -2.504609028350174e6),
+		new Vector(8.173959744337409, 4.180988805185464, -3.975131633740239e-1),
+		5.6834e26,
+		58232000,
+		"saturn",
+		"brown"
+	)
+);
+planets.push(
+	new Planet(
+		new Vector(2.386502033754993e9, 1.755339311618655e9, -2.43923685453279e7),
+		new Vector(-4.07345723288944, 5.172320436045517, 7.190645630817283e-2),
+		86.813e24,
+		25362000,
+		"uranus",
+		"lightblue"
+	)
+);
+for (var p of planets) {
+	//convert from kilometers to meters
+	p.v = p.v.multiply(1000);
+	p.p = p.p.multiply(1000);
+}
 
 setInterval(function() {
 	//ctx.fillStyle = "rgba(0,0,0,0.05)";
 	//ctx.fillRect(0, 0, canvas.width, canvas.height);
+	let max_x = -Infinity,
+		max_y = -Infinity,
+		min_x = Infinity,
+		min_y = Infinity;
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	for (var i = 0; i < 20; i++) {
 		// NOTE: the step size has to stay the same for the entire calculation
-		for (var p of planets) adaptive_rk4_v2(p, dt);
-
-		drawPlanet(sun, 150, "yellow");
-		drawPlanet(mars, 70, "red");
-		drawPlanet(earth, 50, "blue");
-		drawPlanet(venus, 30, "green");
+		for (var p of planets) {
+			adaptive_rk4_v2(p, dt);
+		}
 		adjustTimestep();
 	}
-}, 16);
+	//draw all the planets
+	for (var p of planets) {
+		if (p.p.x > max_x) max_x = p.p.x;
+		if (p.p.y > max_y) max_y = p.p.y;
+		if (p.p.x < min_x) min_x = p.p.x;
+		if (p.p.y < min_y) min_y = p.p.y;
+		drawPlanet(p, p.r, p.color);
+	}
+	//scale the viewport to fit all planets
+	/*origin.scale = Math.min(
+		4e2 /
+			Math.max(
+				Math.abs(max_x),
+				Math.abs(min_y),
+				Math.abs(min_x),
+				Math.abs(max_y)
+			),
+		origin.scale
+	);*/
+	origin.x = 500 / origin.scale;
+	origin.y = 500 / origin.scale;
+	ctx.fillStyle = "#fff";
+	ctx.clearRect(0, 0, 200, 40);
+	ctx.fillText(origin.scale, 10, 10);
+	ctx.fillText(origin.x, 10, 20);
+	ctx.fillText(origin.y, 10, 30);
+}, 7);
 
 function drawPlanet(p, radius, color) {
 	ctx.beginPath();
 	ctx.arc(
-		p.p.x,
-		p.p.y,
-		radius * (10 / Math.max(0.01, p.p.z + 100)),
+		(p.p.x + origin.x) * origin.scale,
+		(p.p.y + origin.y) * origin.scale,
+		radius * origin.planet_scale * origin.scale,
 		0,
 		Math.PI * 2,
 		false
